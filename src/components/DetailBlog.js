51952c67605912eac4b1efb28f90cef84957e005
khuong16 <khuong16>
2022-01-14 14:16:48 +0700

    reusing_custom_hooks_video31

--- src/components/DetailBlog.js
+++ src/components/DetailBlog.js
@@ -1,11 +1,17 @@
 import { useParams, useHistory } from 'react-router-dom';
+import useFetch from '../customize/fetch';
+import './Blog.scss';
 
 const DetailBlog = () => {
 
     let { id } = useParams();
     let history = useHistory();
 
+    const { data: dataBlogDetail, isLoading, isError }
+        = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`, false)
+
     console.log('>>> check use param: ', useParams())
+
     const handleBackData = () => {
         history.push('/blog');
     }
@@ -13,7 +19,19 @@ const DetailBlog = () => {
     return (
         <>
             <div> <span onClick={handleBackData}>&lt;-- Back </span></div>
-            <h1> Hello detail blogs with id = {id}</h1>
+            <div className="blog-detail">
+                {dataBlogDetail &&
+                    <>
+                        <div className="title">
+                            Blog ID: {id} ---  {isLoading === true ? 'Loading data ...' : dataBlogDetail.title}
+                        </div>
+                        <div className="content">
+                            {dataBlogDetail.body}
+                        </div>
+                    </>
+                }
+
+            </div>
         </>
     )
 }
